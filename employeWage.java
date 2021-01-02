46238595d1f7b28576ae6aea43b4eb8830f162cd
sonal <sonalkarle01@gmail.com>
2021-01-02 10:15:54 +0530

    Compute employee wage for multiple companies

--- employeWage.java
+++ employeWage.java
@@ -1,7 +1,44 @@
-class employeWage
+public class employeWage
 {
+	//Compute employee wages for multiple companies
+	//constant
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static void computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
+	{
+	//variables
+	
+		int empHrs=0;
+		int totalWorkingDays=0;
+		int totalEmpHrs=0;
+	//computation
+		while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays <= numOfWorkingDays)
+		{
+			totalWorkingDays++;
+			int empCheck=(int)Math.floor(Math.random()* 10) %3;
+			switch(empCheck)
+			{
+				case IS_FULL_TIME:
+				empHrs=8;
+				break;
+				case IS_PART_TIME:
+				empHrs=4;
+				break;
+				default:
+				empHrs=0;
+				break;
+			}
+		totalEmpHrs += empHrs;
+		System.out.println("Day#: " + totalWorkingDays + " EmpHr: " +empHrs);
+		}
+	int totalEmpWage = totalEmpHrs * empRatePerHour;
+	System.out.println("Total emp wage for company: " +company+"is: "+totalEmpWage );
+
+	}
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee wage program");
+	computeEmpWage("Dmart",20,26,10);
+	computeEmpWage("Reliance",20,25,40);
 	}
+
 }
