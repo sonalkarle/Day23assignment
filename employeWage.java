fbee4665d6f006f7192cda551291fa5c922c6ea8
sonal <sonalkarle01@gmail.com>
2021-01-03 11:48:06 +0530

    Calculate daily employee wage

--- employeWage.java
+++ employeWage.java
@@ -1,7 +1,30 @@
-class employeWage
+class employeeWage
 {
+	//Check employee present or not and if present calculate its daily wage
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee wage program");
+		//variables
+                int isPresent=1;
+                int empRatePerHr=20;
+                int salary=0;
+                int empHr=0;
+		//computation
+                int res=(int)(Math.random()*10)%2;
+                //condition
+		if(res==isPresent)
+                {
+                        System.out.println("Employee is present");
+                        empHr=8;
+                }
+                else
+                {
+                        System.out.println("Employee is absent");
+                        empHr=0;
+                }
+		//final answer
+                salary=(empRatePerHr*empHr);
+		System.out.println("Salary is " +salary);		
+
+
 	}
 }
