768a2ed5402a036f46b4f210b5248bd3160f5467
sonal <sonalkarle01@gmail.com>
2021-01-03 11:45:24 +0530

    Check employee present or absent

--- employeWage.java
+++ employeWage.java
@@ -1,7 +1,20 @@
-class employeWage
+class employeeWage
 {
+	//Check employee is present or not
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to employee wage program");
+		//constant
+                int isPresent=1;
+		//computation
+                int res=(int)(Math.random()*10)%2;
+                if(res==isPresent)
+                {
+                        System.out.println("Employee is present");
+                }
+                else
+                {
+                        System.out.println("Employee is absent");
+                }
+
 	}
 }
